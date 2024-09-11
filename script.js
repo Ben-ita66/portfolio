a045fe376b1e8d3f5b1ee3515301e6a30ce9deff
Ben-ita66 <benitanwabueze72@gmail.com>
2024-09-11 18:20:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+tailwind.config = {
+    theme: {
+      screens: {
+          sm: '480px',
+          md: '768px',
+          lg: '976px',
+          xl: '1440px',
+        },      
+      extend: {
+        colors: {
+          clifford: '#da373d',
+            customColor: 'rgb(136,146,176)', 
+            bluey: 'rgb(113,175,246)' ,
+            parad : '#d8dff8',  
+            greenO: '#a5a550'  ,
+            greenish: '#1a381d',
+            pro: '#11151d',
+        }
+        
+      }
+    }
+  }
+
+// const bar2 = getElementById('#bar2')
+//     bar2.addEventListener('click', function() {
+//       // Toggle background color between white and blue-500
+//       bar2.classList.toggle('bg-blue-500');
+//       bar2.classList.toggle('bg-white'); // Ensure you have a `bg-white` class in Tailwind
+    
+//       // Toggle rotation
+//       bar2.classList.toggle('rotate-90');
+//     });
+    
+
+  
+//     const fullScreenSections = document.getElementById('fullScreenSections');
+    
+//     bar2.addEventListener('click', function() {
+//       // Toggle the full-screen class and the hidden state
+//       fullScreenSections.classList.toggle('full-screen');
+//       fullScreenSections.classList.toggle('hidden');
+//     });
+  
+
+
+    
+      
+      // Vertical lines transition
+    //   window.addEventListener('load', () => {
+    //     setTimeout(() => {
+    //         const section1 = document.getElementById('section1');
+    //         section1.style.transform = 'translateY(0)';
+    //     }, 300);
+    
+    //     setTimeout(() => {
+    //         const section2 = document.getElementById('section2');
+    //         section2.style.transform = 'translateY(0)';
+    //     }, 600);
+    
+    //     setTimeout(() => {
+    //         const section3 = document.getElementById('section3');
+    //         section3.style.transform = 'translateY(0)';
+    //     }, 900);
+    // });
+    
